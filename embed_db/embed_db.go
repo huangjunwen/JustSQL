d43f37cd44f1be819309bc7a6035dd62decbbffa
jayven <kassarar@gmail.com>
2017-06-27 18:22:34 +0800

    Seperate some functions in embed_db

--- embed_db/embed_db.go
+++ embed_db/embed_db.go
@@ -6,6 +6,7 @@ import (
 	"github.com/pingcap/tidb/context"
 	"github.com/pingcap/tidb/expression"
 	"github.com/pingcap/tidb/kv"
+	"github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/plan"
 	"github.com/pingcap/tidb/sessionctx"
 )
@@ -25,17 +26,15 @@ func NewEmbedDB(store_path string) (*EmbedDB, error) {
 
 	// Use memory store by default
 	if store_path == "" {
-		store_path = "memory://"
+		store_path = tidb.EngineGoLevelDBMemory
 	}
 
 	if store, err = tidb.NewStore(store_path); err != nil {
 		return nil, err
 	}
-
 	if _, err = tidb.BootstrapSession(store); err != nil {
 		return nil, err
 	}
-
 	if sess, err = tidb.CreateSession(store); err != nil {
 		return nil, err
 	}
@@ -47,44 +46,78 @@ func NewEmbedDB(store_path string) (*EmbedDB, error) {
 
 }
 
-func (db *EmbedDB) Execute(src string) ([]ast.RecordSet, error) {
-	return db.Sess.Execute(src)
+func (db *EmbedDB) parse(src string) ([]ast.StmtNode, error) {
+	ctx := db.Sess.(context.Context)
+	p := parser.New()
+	p.SetSQLMode(ctx.GetSessionVars().SQLMode)
+	charset, collation := ctx.GetSessionVars().GetCharsetInfo()
+	return p.Parse(src, charset, collation)
 }
 
-func (db *EmbedDB) Compile(src string) ([]ast.StmtNode, error) {
-	var (
-		stmts []ast.StmtNode
-		err   error
-	)
+func (db *EmbedDB) parseOne(src string) (ast.StmtNode, error) {
+	stmts, err := db.parse(src)
+	if err != nil {
+		return nil, err
+	}
+	if len(stmts) != 1 {
+		return nil, parser.ErrSyntax
+	}
+	return stmts[0], nil
+}
 
+func (db *EmbedDB) infer(stmt ast.StmtNode) error {
 	ctx := db.Sess.(context.Context)
 	is := sessionctx.GetDomain(ctx).InfoSchema()
 
-	// Parse SQL statements.
-	if stmts, err = tidb.Parse(ctx, src); err != nil {
+	// This is part of tidb.Compile. Also refer expression/typeinferer_test.go
+
+	// Resolve column names and table names, ResultSetNode types' ResultFields
+	// are generated in this step.
+	if err := plan.ResolveName(stmt, is, ctx); err != nil {
+		return err
+	}
+	// Validate the statement.
+	if err := plan.Validate(stmt, false); err != nil {
+		return err
+	}
+	// Infer expression types, like 'count(*) AS cnt'
+	if err := expression.InferType(ctx.GetSessionVars().StmtCtx, stmt); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Parse then infer names and types for SQL.
+func (db *EmbedDB) Compile(src string) ([]ast.StmtNode, error) {
+	stmts, err := db.parse(src)
+	if err != nil {
 		return nil, err
 	}
 
-	// The loop is part of the process of tidb.Compile. Also refer expression/typeinferer_test.go
 	for _, stmt := range stmts {
-		// Resolve column names and table names, ResultSetNode types' ResultFields
-		// are generated in this step.
-		if err = plan.ResolveName(stmt, is, ctx); err != nil {
+		if err := db.infer(stmt); err != nil {
 			return nil, err
 		}
-
-		// Validate the statement.
-		if err = plan.Validate(stmt, false); err != nil {
-			return nil, err
-		}
-
-		// Infer expression types, like 'count(*) AS cnt'
-		if err = expression.InferType(ctx.GetSessionVars().StmtCtx, stmt); err != nil {
-			return nil, err
-		}
-
 	}
 
 	return stmts, nil
+}
 
+// Parse then infer names and types for SQL for single statement.
+func (db *EmbedDB) CompileOne(src string) (ast.StmtNode, error) {
+	stmt, err := db.parseOne(src)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.infer(stmt); err != nil {
+		return nil, err
+	}
+
+	return stmt, nil
+}
+
+// Execute some SQLs.
+func (db *EmbedDB) Execute(src string) ([]ast.RecordSet, error) {
+	return db.Sess.Execute(src)
 }
