5bfb1807ebe0bfa2014e0f570a0361870cb56e64
jayven <kassarar@gmail.com>
2017-07-25 10:25:57 +0800

    Add dml_meta.go

---
+++ context/dml_meta.go
@@ -0,0 +1,94 @@
+package context
+
+import (
+	"fmt"
+	"github.com/huangjunwen/JustSQL/utils"
+	"github.com/pingcap/tidb/ast"
+	"github.com/pingcap/tidb/util/types"
+)
+
+type ResultFieldMeta struct {
+	*ast.ResultField
+
+	// Field name
+	Name       string
+	PascalName string
+
+	// Field type.
+	Type types.FieldType
+
+	// Go type to store this field.
+	AdaptType *TypeName
+
+	// If this is a field from table column.
+	// The following fields have values.
+	Table  *TableMeta
+	Column *ColumnMeta
+}
+
+func newResultFieldMeta(ctx *Context, rf *ast.ResultField) (*ResultFieldMeta, error) {
+
+	db_meta, err := ctx.DBMeta()
+	if err != nil {
+		return nil, err
+	}
+
+	// Is it from a real table?
+	var table *TableMeta = nil
+	var column *ColumnMeta = nil
+	table, _ = db_meta.Tables[rf.Table.Name.L]
+	if table != nil {
+		column = table.Columns[rf.Column.Offset]
+	}
+
+	// Determin the name.
+	name := rf.ColumnAsName.L
+	if name == "" {
+		name = rf.Column.Name.L
+	}
+	if name == "" {
+		return nil, fmt.Errorf("No name for *ast.ResultField")
+	} else if !utils.IsIdent(name) {
+		name = utils.FindIdent(name)
+		if name == "" {
+			return nil, fmt.Errorf(
+				"Can't find a valid identifier in %+q, you can add 'AS alias'", name)
+		}
+	}
+
+	return &ResultFieldMeta{
+		ResultField: rf,
+		Name:        name,
+		PascalName:  utils.PascalCase(name),
+		Type:        rf.Column.FieldType,
+		AdaptType:   ctx.TypeAdapter.AdaptType(&rf.Column.FieldType),
+		Table:       table,
+		Column:      column,
+	}, nil
+
+}
+
+type SelectStmtMeta struct {
+	*ast.SelectStmt
+
+	ResultFields []*ResultFieldMeta
+}
+
+func NewSelectStmtMeta(ctx *Context, stmt *ast.SelectStmt) (*SelectStmtMeta, error) {
+
+	rfs := stmt.GetResultFields()
+	ret := &SelectStmtMeta{
+		SelectStmt:   stmt,
+		ResultFields: make([]*ResultFieldMeta, 0, len(rfs)),
+	}
+	for _, rf := range rfs {
+		rfm, err := newResultFieldMeta(ctx, rf)
+		if err != nil {
+			return nil, err
+		}
+		ret.ResultFields = append(ret.ResultFields, rfm)
+	}
+
+	return ret, nil
+
+}
--- utils/utils.go
+++ utils/utils.go
@@ -6,20 +6,30 @@ import (
 	"unicode"
 )
 
-var ident_re *regexp.Regexp = regexp.MustCompile(`^[A-Za-z][A-Za-z0-9_]*$`)
+var (
+	ident_re       *regexp.Regexp = regexp.MustCompile(`[^A-Za-z]*([A-Za-z])([A-Za-z0-9]*)`)
+	exact_ident_re *regexp.Regexp = regexp.MustCompile(`^[A-Za-z][A-Za-z0-9_]*$`)
+)
 
 // Is it a valid identifier?
 func IsIdent(s string) bool {
-	return ident_re.MatchString(s)
+	return exact_ident_re.MatchString(s)
 }
 
-var word_re *regexp.Regexp = regexp.MustCompile(`[^A-Za-z]*([A-Za-z])([A-Za-z0-9]*)`)
+// Find left most identifier.
+func FindIdent(s string) string {
+	m := ident_re.FindStringSubmatch(s)
+	if m == nil {
+		return ""
+	}
+	return m[1] + m[2]
+}
 
 // Convert a string to pascal case. Example: "pascal_case" -> "PascalCase"
 func PascalCase(s string) string {
 	parts := []string{}
-	for _, m := range word_re.FindAllStringSubmatch(s, -1) {
-		parts = append(parts, strings.ToUpper(m[1]), m[2])
+	for _, m := range ident_re.FindAllStringSubmatch(s, -1) {
+		parts = append(parts, strings.ToUpper(m[1]), strings.ToLower(m[2]))
 	}
 	return strings.Join(parts, "")
 }
