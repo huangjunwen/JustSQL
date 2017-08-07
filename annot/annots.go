ea297313fcb1b9b69a368e996d5271608017baa7
jayven <kassarar@gmail.com>
2017-08-07 09:12:55 +0800

    Merge annot_meta.go into annots.go

--- annot/annots.go
+++ annot/annots.go
@@ -3,7 +3,38 @@ package annot
 import (
 	"fmt"
 	"github.com/huangjunwen/JustSQL/utils"
+	"strings"
 )
+
+// Global settings.
+var (
+	BindNamePrefix string = ":"
+)
+
+// Declare a global setting annotation.
+type SettingAnnot struct{}
+
+func (a *SettingAnnot) SetPrimary(val string) error {
+	if val != "" {
+		return fmt.Errorf("setting: expect no primary value but got %+q", val)
+	}
+	return nil
+}
+
+func (a *SettingAnnot) Set(key, val string) error {
+	switch key {
+	default:
+		return fmt.Errorf("setting: unknown option %+q", key)
+	case "bindNamePrefix":
+		if val == "" {
+			return fmt.Errorf("setting: got empty setting for bindNamePrefix")
+		}
+		BindNamePrefix = val
+	case "":
+		return nil
+	}
+	return nil
+}
 
 // SubsAnnot declares a block of content for substitution directly.
 type SubsAnnot struct {
@@ -133,36 +164,106 @@ func (a *BindAnnot) Set(key, val string) error {
 	return nil
 }
 
-// Declare a bind option. Defualt to ':'.
-type BindOptAnnot struct {
-	// Prefix of named placeholder in query string.
-	NamePrefix string
-}
-
-func (a *BindOptAnnot) SetPrimary(val string) error {
-	if val != "" {
-		return fmt.Errorf("bindOpt: expect no primary value but got %+q", val)
-	}
-	return nil
-}
-
-func (a *BindOptAnnot) Set(key, val string) error {
-	switch key {
-	default:
-		return fmt.Errorf("bindOpt: unknown option %+q", key)
-	case "namePrefix":
-		if val == "" {
-			return fmt.Errorf("bindOpt: got empty setting for namePrefix")
-		}
-		a.NamePrefix = val
-	case "":
-		return nil
-	}
-	return nil
-}
-
+// Regist Annotations.
 func init() {
+	RegistAnnot((*SettingAnnot)(nil), "setting")
 	RegistAnnot((*FuncAnnot)(nil), "func")
 	RegistAnnot((*ArgAnnot)(nil), "arg", "param")
 	RegistAnnot((*BindAnnot)(nil), "bind")
+}
+
+// AnnotMeta contains meta information of annotations.
+type AnnotMeta struct {
+	// Source query text.
+	SrcText string
+
+	// Processed query text (e.g. comments stripped and annotations processed).
+	Text string
+
+	// Comments (and annotations) in SrcQuery.
+	Comments []Comment
+
+	// Function name (from FuncAnnot).
+	FuncName string
+
+	// Function arguments (from ArgAnnot).
+	Args []*ArgAnnot
+
+	// Return style.
+	ReturnStyle
+
+	// List of binding name.
+	Bindings []string
+
+	// "IN (?)" bindings (index of Bindings)
+	InBindings []int
+}
+
+var noNameCnt int = 0
+
+// NewAnnotMeta gather wrapper meta from source query's comments (annotations).
+func NewAnnotMeta(src string) (*AnnotMeta, error) {
+
+	ret := &AnnotMeta{
+		SrcText:    src,
+		Args:       make([]*ArgAnnot, 0),
+		Bindings:   make([]string, 0),
+		InBindings: make([]int, 0),
+	}
+
+	comments, err := ScanComment(src)
+	if err != nil {
+		return nil, err
+	}
+	ret.Comments = comments
+
+	parts := []string{}
+	offset := 0
+	for i := 0; i < len(comments); i++ {
+		comment := comments[i]
+
+		// Append text before the comment.
+		parts = append(parts, src[offset:comment.Offset])
+
+		// For different annotations.
+		switch a := comment.Annot.(type) {
+		case *SubsAnnot:
+			parts = append(parts, a.Content)
+
+		case *FuncAnnot:
+			ret.FuncName = a.Name
+			ret.ReturnStyle = a.ReturnStyle
+
+		case *ArgAnnot:
+			ret.Args = append(ret.Args, a)
+
+		case *BindAnnot:
+			// Find the next comment.
+			i += 1
+			if i >= len(comments) {
+				return nil, fmt.Errorf("bind: %q missing enclosure", a.Name)
+			}
+			parts = append(parts, BindNamePrefix+a.Name)
+			if a.In {
+				ret.InBindings = append(ret.InBindings, len(ret.Bindings))
+			}
+			ret.Bindings = append(ret.Bindings, a.Name)
+			comment = comments[i]
+
+		default:
+		}
+
+		offset = comment.Offset + comment.Length
+	}
+
+	parts = append(parts, src[offset:])
+	ret.Text = strings.Trim(strings.Join(parts, ""), " \t\n\r;")
+
+	if ret.FuncName == "" {
+		noNameCnt += 1
+		ret.FuncName = fmt.Sprintf("NoName%d", noNameCnt)
+	}
+
+	return ret, nil
+
 }
